9d2f6b28f00caf9e0e687d48d0046f5d2bd28c8e
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-07-04 19:06:16 +0000

    style bleu et or

--- my-task-manager-app/frontend/src/pages/LoginPage.jsx
+++ my-task-manager-app/frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useContext } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { Link, useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { login } = useContext(AuthContext);
-  const navigate = useNavigate(); // Utiliser useNavigate
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
@@ -20,9 +21,8 @@ const LoginPage = () => {
 
       if (response.ok) {
         const data = await response.json();
-        login(data.token, data.user); // Assurez-vous que le login prend deux arguments : token et user
-        console.log('Utilisateur connecté:', data);
-        navigate('/'); // Rediriger vers la page d'accueil après la connexion
+        login(data.token, data.user);
+        navigate('/');
       } else {
         console.error('Échec de la connexion', response.status, response.statusText);
       }
@@ -32,27 +32,31 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
-      <h1>Connexion</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        autoComplete="email"
-      />
-      <input
-        type="password"
-        placeholder="Mot de passe"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        autoComplete="current-password"
-      />
-      <button onClick={handleLogin}>Se connecter</button>
-      <p>
-        Pas encore de compte ? <Link to="/register">Créer un compte</Link>
-      </p>
-    </div>
+    <main>
+      <div className="login-container">
+        <div className="login-box">
+          <h1>Connexion</h1>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoComplete="email"
+          />
+          <input
+            type="password"
+            placeholder="Mot de passe"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="current-password"
+          />
+          <button onClick={handleLogin}>Se connecter</button>
+          <p>
+            Pas encore de compte ? <Link to="/register">Créer un compte</Link>
+          </p>
+        </div>
+      </div>
+    </main>
   );
 };
 

ac6abeb6a720159ca0af03ee49518aa2c7c2b521
CappieGold <144028326+CappieGold@users.noreply.github.com>
2024-07-04 19:59:19 +0000

    footer

--- my-task-manager-app/frontend/src/main.jsx
+++ my-task-manager-app/frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './pages/LoginPage.css';
+import './components/Footer.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
